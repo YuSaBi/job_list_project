491259711bcbfb704aabe0bca5ce9804323dd9e7
oguzcan baş <oguzcan@seykol.com>
2022-08-11 12:47:01 +0300

    test_2_navigator_düzeltilecek

--- lib/views/loginScreen.dart
+++ lib/views/loginScreen.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-//import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
 //import 'package:job_list_project/models/userLogin.dart';
 //import 'package:job_list_project/models/userLoginRequest.dart';
@@ -11,10 +11,10 @@ import '../models/userLoginResponse.dart';
 
 // future post method
 Future<LoginResponseModel> userLogin(String name, String password) async {
-  //var jsonData;
-  //SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+  var jsonData;
+  SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
   final response = await http.post(
-    Uri.parse('http://192.168.2.172:8080/api/Default/UserLogin_Manager'),// 10.0.2.2   localhost  Mert : 192.168.177.172  Test_UserLogin
+    Uri.parse('http://10.0.2.2:8080/api/Default/UserLogin_Manager'),// 10.0.2.2   localhost  Mert : 192.168.177.172  Test_UserLogin
     headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8',
     },
@@ -26,21 +26,23 @@ Future<LoginResponseModel> userLogin(String name, String password) async {
 
   if (response.statusCode == 200) {
     print("Body'miz bu şekilde: "+response.body);
-    //jsonData = json.decode(response.body);
-    return LoginResponseModel.fromJson(jsonDecode(response.body));
-    /*
-    if (jsonData['UserID']==null) {
-      print("HATA: !!! UserID boş geldi :( ");
-    } else if(jsonData['UserID']==0){
-      print("Kullanıcı adı veya şifre hatalı.");
-    } else{
-      sharedPreferences.setString("UserID", jsonData['UserID']);
-    }
-    */
+    jsonData = json.decode(response.body);
     
+    if (jsonData['userID']==null) {
+      print("HATA: !!! userID boş geldi :( ");
+      sharedPreferences.setString("responseMsg", "HATA: !!! userID boş geldi :( ");
+    } else if(jsonData['userID']==0){
+      print("Kullanıcı adı veya şifre hatalı.");
+      sharedPreferences.setString("responseMsg", "Kullanıcı adı veya şifre hatalı.");
+    } else{
+      sharedPreferences.setString("responseMsg", "Giriş başarılı");
+      sharedPreferences.setString("userID", jsonData['userID'].toString());
+      sharedPreferences.setString("userName", name);
+    }
+    return LoginResponseModel.fromJson(jsonDecode(response.body));
   } else {
-    print("net hatası olabilir");
-    throw Exception('net hatası olabilir. ${response.body}');
+    print("net hatası olabilir, bağlantı hatası");
+    throw Exception('net hatası olabilir.');
   }
 }
 
@@ -58,8 +60,9 @@ class _loginScreenState extends State<loginScreen> {
   final TextEditingController _usernameController=new TextEditingController();
   final TextEditingController _passwordController=new TextEditingController();
   Future<LoginResponseModel>? _futureUserLogin;
-  String username = "";
-  String password = "";
+  //Future<int>? _durum;
+  //String username = "";
+  //String password = "";
   final _formKey = GlobalKey<FormState>();
   bool _isLoading = false;
 
@@ -77,25 +80,26 @@ class _loginScreenState extends State<loginScreen> {
   }
   
   buildBody() {
+    double paddingLogin =20.0;
     return Form(
       // key verilecek
-      child: Padding(
-        padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-        child: (_futureUserLogin == null) ? buildColumn() : buildFutureBuilder()
-      ),
+      child: (_futureUserLogin == null) ? buildColumn() : buildFutureBuilder()
     );
   }
 
   buildColumn() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        buildUsernameField(),
-        const SizedBox(height: 10.0,),
-        buildPasswordField(),
-        SizedBox(height: 30.0,),
-        buildLoginButton(),
-      ],
+    return Padding(
+      padding: const EdgeInsets.only(left: 20.0,right: 20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          buildUsernameField(),
+          const SizedBox(height: 10.0,),
+          buildPasswordField(),
+          SizedBox(height: 30.0,),
+          buildLoginButton(),
+        ],
+      ),
     );
   }
 
@@ -111,9 +115,11 @@ class _loginScreenState extends State<loginScreen> {
           return null;
         }
       },
+      /*
       onSaved: (String? newValue) {
         username = newValue!;
       },
+      */
     );
   }
   
@@ -132,9 +138,11 @@ class _loginScreenState extends State<loginScreen> {
           return null;
         }
       },
+      /*
       onSaved: (String? newValue) {
         password=newValue!;
       },
+      */
     );
   }
   
@@ -161,15 +169,17 @@ class _loginScreenState extends State<loginScreen> {
   }
   
   FutureBuilder<LoginResponseModel> buildFutureBuilder() {
-    return FutureBuilder<LoginResponseModel>(
+    return FutureBuilder(
       future: _futureUserLogin,
       builder: (context, snapshot) {
         if(snapshot.hasData){
           //return Text(snapshot.data!.responseMsg);
           if (snapshot.data!.responseCode==1) {
             print("giriş sayfasına yönlendiriliyorsunuz...");
-            return Text(snapshot.data!.responseMsg);
-            //Navigator.pushAndRemoveUntil(context, newRoute, (route) => false)
+            //return Text(snapshot.data!.responseMsg);
+            //Navigator.pushAndRemoveUntil(context, newRoute, (route) => false) /// BURADAN YENİ SAYFAYA GİDİLECEK
+            //GİDERKEN YANINDA LOGİNRESPONSE CLASSINDAN ÖRNEK GOTURECEK SNAPSHOT DATA İLE
+            return buildMainScreen();
           }else{
             print("responsecode 1 değil");
             return Text("Yanlış girdiniz.");
@@ -182,6 +192,100 @@ class _loginScreenState extends State<loginScreen> {
       },
     );
   }
+  
+  buildMainScreen() {
+    return Stack(
+        //key: _formKey,
+      children: <Widget>[
+        Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color.fromARGB(255, 140, 167, 181),//Color(0xFF84FDD9),//Color(0xFF73AEF5),
+                  Color.fromARGB(255, 108, 142, 159),//Color(0xFF60E6BE),//  Color(0xFF61A4F1),
+                  Color.fromARGB(255, 107, 141, 155),//Color(0xFF72F5CE),// Color(0xFF478DE0),
+                  Color.fromARGB(255, 45, 92, 114),//Color(0xFF1AB7AC),// Color(0xFF398AE5),
+                ],
+                stops: [
+                  0.1,
+                  0.4,
+                  0.7,
+                  0.9
+                ]),
+          ),
+        ),
+        Container(
+          height: double.infinity,
+          child: SingleChildScrollView(
+            physics: const AlwaysScrollableScrollPhysics(),
+            padding: const EdgeInsets.symmetric(
+              horizontal: 40.0,
+              vertical: 300.0,
+            ),
+            //key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                
+                SizedBox(
+                  height: 10.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                ),
+                //_loginButton()
+                const SizedBox(
+                  height: 30.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ),
+                _IsGirisBtn(),
+                const SizedBox(
+                  height: 30.0,
+                ),
+                _IsListeleBtn(),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+  
+  //ButtonTheme 
+
+  Widget _IsGirisBtn() => ButtonTheme(
+    child: ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        primary: Colors.teal,
+        onSurface: Colors.amber,
+        minimumSize: Size(100.0, 40.0)
+      ),
+      child: Text("İş ekleme"),
+      onPressed: () {
+        return null;
+      },
+    ),
+  );
+      
+  Widget _IsListeleBtn() => ButtonTheme(
+    child: ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        primary: Colors.teal,
+        onSurface: Colors.amber,
+        minimumSize: Size(100.0, 40.0)
+      ),
+      child: Text("İşleri listele"),
+      onPressed: () {
+        return null;
+      },
+    ),
+  );
   
   /*
   Future<UserLoginResponseModel> login(String username, String userpassword) async{
