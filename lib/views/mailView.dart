040077bbbe5319bf29633af92318ba36b3a345f4
Oguzcan Bas <oguzcan@seykol.com>
2022-09-01 11:21:23 +0300

    mail modülü oluşturulmaya başlandı 2

---
+++ lib/views/mailView.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:job_list_project/models/httpConfig.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+/// MAIN CLASS ///
+class mailView extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _mailViewState();
+  }
+  
+}
+
+/// MAIN STATE ///
+class _mailViewState extends State{
+  bool _isLoading = false;
+  late SharedPreferences sharedPreferences;
+  int userID = 0;
+  late var veriler;
+
+  @override
+  void initState() {
+    setState(() {
+      _isLoading = true;
+      getMails(userID);
+    });
+    super.initState();
+  }
+
+  void getMails(int userID) async {
+    var jsonData;
+
+    while (true) {
+      if (userID == 0) {
+        try {
+          sharedPreferences = await SharedPreferences.getInstance();
+          userID = int.parse(sharedPreferences.getString("userID").toString());
+        } catch (e) {
+          print("hata: "+ e.toString());
+          print("shared pref ile ilgili hata var {void getMails, Line 30 +-10}");
+        }
+      } else {
+        try {
+          postMethodConfig config = postMethodConfig();
+          //final Response = await http.post()
+        } catch (e) {
+          
+        }
+      }
+    }
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Mail"),
+        backgroundColor: Colors.teal.shade300,
+        centerTitle: true,
+      ),
+      body: buildBody(),
+    );
+  }
+  
+  buildBody() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: _isLoading
+      ? const Center(
+        child:  CircularProgressIndicator(),
+      )
+      : Column(
+        children: <Widget>[
+          Expanded(child: builderListView()),
+          const SizedBox(
+            height: 5.0,
+          ),
+        ],
+      )
+    );
+  }
+  
+  builderListView() {
+    return RefreshIndicator(
+      onRefresh: refreshList(),
+      child: ListView.builder(
+        itemCount: veriler == "empty" ? 1 : veriler.length ,
+        itemBuilder: (context, index) {
+          return _isLoading
+          ? Center(
+              child: CircularProgressIndicator(
+                backgroundColor: Colors.grey.shade400
+              ),
+          )
+          : Column(
+            children: [
+              veriler == "empty" || veriler == null || veriler==[]
+                ? const Text(
+                  "Henüz bir mail almadınız."
+                )
+                : const Text("Mail almışsınız, ekrana yazdırılacak") // gelen mailler ekranda listelenecek
+            ],
+          );
+        },
+      ),
+    );
+  }
+  
+  refreshList() {
+    return null;
+  }
+
+}
--- lib/views/mainScreen.dart
+++ lib/views/mainScreen.dart
@@ -59,6 +59,11 @@ class _MainScreenState extends State<MainScreen> {
               const PopupMenuItem(
                 value: 0,
                 child: Text("Mail"),
+              ),
+              PopupMenuDivider(),
+              const PopupMenuItem(
+                value: 1,
+                child: Text("Çıkış"),
               )
             ]
           )
@@ -77,7 +82,7 @@ class _MainScreenState extends State<MainScreen> {
 
   void popUpOnSelected(BuildContext context, int item){
     switch (item){
-      case 0:
+      case 1:
         sharedPreferences.setBool('isLogged', false);
         Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context) => loginScreen()), (Route<dynamic> route) => false);
     }
